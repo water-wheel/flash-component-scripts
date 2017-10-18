ec745f0910b15572164738324c9107d3815e5764
fengdewang <fengdewang@58ganji.com>
2017-10-18 17:16:10 +0800

    init

---
+++ config/paths.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebookincubator/create-react-app/issues/637
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const envPublicUrl = process.env.PUBLIC_URL;
+
+function ensureSlash(path, needsSlash) {
+    const hasSlash = path.endsWith('/');
+    if (hasSlash && !needsSlash) {
+        return path.substr(path, path.length - 1);
+    } else if (!hasSlash && needsSlash) {
+        return `${path}/`;
+    } else {
+        return path;
+    }
+}
+
+const getPublicUrl = appPackageJson =>
+envPublicUrl || require(appPackageJson).homepage;
+
+// We use `PUBLIC_URL` environment variable or "homepage" field to infer
+// "public path" at which the app is served.
+// Webpack needs to know it to put the right <script> hrefs into HTML even in
+// single-page apps that may serve index.html for nested URLs like /todos/42.
+// We can't use a relative path in HTML because we don't want to load something
+// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
+function getServedPath(appPackageJson) {
+    const publicUrl = getPublicUrl(appPackageJson);
+    const servedUrl =
+        envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+    return ensureSlash(servedUrl, true);
+}
+
+//add flexible
+const flexibleStr = (function(){
+    return fs.readFileSync('node_modules/flash-flexible/build/changeRem-min.js', 'utf-8');
+})();
+
+// config after eject: we're in ./config/
+module.exports = {
+    dotenv: resolveApp('.env'),
+    appPublic: resolveApp('demo'),
+    appHtml: resolveApp('demo/index.html'),
+    appIndexJs: resolveApp('demo/index.js'),
+    appPackageJson: resolveApp('package.json'),
+    appSrc: resolveApp('demo'),
+    componentIndexJs: resolveApp('./src'),
+    yarnLockFile: resolveApp('yarn.lock'),
+    testsSetup: resolveApp('demo/setupTests.js'),
+    appNodeModules: resolveApp('node_modules'),
+    publicUrl: getPublicUrl(resolveApp('package.json')),
+    // servedPath: getServedPath(resolveApp('package.json')),
+    servedPath: './',
+
+    flexibleStr: flexibleStr,
+
+    // todo: 自定义生产环境中build输出位置及cdn地址
+    appBuild: resolveApp('build') // build输出位置
+
+};
